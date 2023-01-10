b5209b039c1066deee97e72f2eb7496f8e126adb
gayeongogo <rkdus1357@gmail.com>
2023-01-10 23:48:08 +0900

    메인 슬라이더 추가

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ $('.appbarBt').click(function(){
 $('.appbarCloseBt').click(function(){
     $('.mobile-menu').stop().animate({left:'-100%'},500);
 });
+
+/* 메인 슬라이더 */
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    auto: true,
+    speed: 500,
+    pause: 4000,
+    slideWidth: 600
+  });
+});
